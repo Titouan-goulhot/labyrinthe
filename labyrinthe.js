91ef6022c492525e1e02d09746ee8d91ed92b9e8
Titouan Goulhot <73955271+Titouan-goulhot@users.noreply.github.com>
2021-07-25 19:57:33 +0200

    Add files via upload

---
+++ labyrinthe.js
@@ -0,0 +1,25 @@
+var lab = new Array(3)
+
+for (i=0; i < 3; i++){
+      lab[i] = new Array(3)
+}
+
+lab[0][0] = ""
+lab[0][1] = ""
+lab[0][2] = ""
+lab[0][3] = ""
+lab[0][4] = ""
+
+lab[1][0] = ""
+lab[1][1] = ""
+lab[1][2] = ""
+lab[1][3] = ""
+lab[1][4] = ""
+
+lab[2][0] = ""
+lab[2][1] = ""
+lab[2][2] = ""
+lab[2][3] = ""
+lab[2][4] = ""
+
+console.log(lab[0][0]);
